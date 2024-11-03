8ecc04db242fc0a9994c0e6c526dcf43a7d295ca
sf <sf@DESKTOP-5IIPQ9M>
2024-11-03 10:56:15 +0000

    Dodaj pliki projektów.

---
+++ MVVMFirma/ViewModels/NowaKsiazkaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.ViewModels
+{
+    public class NowaKsiazkaViewModel: WorkspaceViewModel
+    {
+        public NowaKsiazkaViewModel() 
+        {
+            base.DisplayName = "Książka";
+        }
+    }
+}
---
+++ MVVMFirma/Views/NowyAdresView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMFirma.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NowyAdresView.xaml
+    /// </summary>
+    public partial class NowyAdresView : UserControl
+    {
+        public NowyAdresView()
+        {
+            InitializeComponent();
+        }
+    }
+}
