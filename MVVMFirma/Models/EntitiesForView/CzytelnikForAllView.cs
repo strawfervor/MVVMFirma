a901fe9600822456cd382dec17c5b5c628890f65
strawfervor <strawfervor@gmail.com>
2024-11-11 17:14:01 +0000

    Edytowana WszyscyCzytelnicyViewModel dodając doń zapytanie linq oraz utworzono CzytelnikForAllView

---
+++ MVVMFirma/Models/EntitiesForView/CzytelnikForAllView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.EntitiesForView
+{
+    //ta klasa jest wzorowana na klasie faktura, tylko zamiast kluczy obcych ma czytelne dla klienta pola (klucze obce będą zastąpione czytelnymi dla zwykłego człowieka danymi).
+    public class CzytelnikForAllView
+    {
+        public int IdCzytelnika { get; set; }
+        public String Imie { get; set; }
+        public String Nazwisko { get; set; }
+
+        //zamiast IdAdresu (klucza obcego) decydujemy jakie pola z Adresu wyświetlić i zamieniamy je (weźniemy tutaj ulica, numer domu i miasto) (49:44)
+        public String AdresUlica { get; set; }
+
+        public String AdresNumerDomu { get; set; }
+
+        public String AdresMiejscowosc { get; set; }
+
+        //zamista IdRodzajuCzlonokostwa  - NazwaCzlonkowstwa
+
+        public String RodzajeCzlonkostwaNazwaCzlonkowstwa {  get; set; }
+
+
+        public int? IloscWypozyczonychKsiazek { get; set; }
+    }
+
+    
+}
--- MVVMFirma/ViewModels/WszyswcyCzytelnicyViewModel.cs
+++ MVVMFirma/ViewModels/WszyswcyCzytelnicyViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszyswcyCzytelnicyViewModel : WszystkieViewModel<Czytelnicy>
+    public class WszyswcyCzytelnicyViewModel : WszystkieViewModel<CzytelnikForAllView>
     {
         #region Constructor
 
@@ -22,9 +23,21 @@ namespace MVVMFirma.ViewModels
         #region Helpers
         public override void Load()
         {
-            List = new ObservableCollection<Czytelnicy>
+            List = new ObservableCollection<CzytelnikForAllView>
                 (
-                   bibliotekaEntities.Czytelnicy.ToList()
+                   //tworzymy tutaj zapytanie linq
+                   from czytelnik in bibliotekaEntities.Czytelnicy //dla każdegi czytelnika z bazy danych czytelników
+                   select new CzytelnikForAllView //tworzymy nowe CzytelnikForAllView i uzupełniamy jego dane
+                   {
+                       IdCzytelnika = czytelnik.Id,
+                       Imie = czytelnik.Imie,
+                       Nazwisko = czytelnik.Nazwisko,
+                       AdresUlica = czytelnik.Adres.Ulica, //odwołanie do Adresu
+                       AdresNumerDomu = czytelnik.Adres.NumerDomu,
+                       AdresMiejscowosc = czytelnik.Adres.Miejscowosc,
+                       RodzajeCzlonkostwaNazwaCzlonkowstwa = czytelnik.RodzajCzlonkostwa.NazwaCzlonkowstwa,
+                       IloscWypozyczonychKsiazek = czytelnik.IloscWypozyczonychKsiazek,
+                   }
                 );
         }
         #endregion
