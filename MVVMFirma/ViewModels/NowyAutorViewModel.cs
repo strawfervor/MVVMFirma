c9fc242716aefc9c4dfd7ebec2fbcb5e70c978c3
strawfervor <strawfervor@gmail.com>
2025-01-07 16:06:27 +0000

    dodano pola do walidowania

--- MVVMFirma/ViewModels/NowyAutorViewModel.cs
+++ MVVMFirma/ViewModels/NowyAutorViewModel.cs
@@ -2,20 +2,24 @@
 using MVVMFirma.Models.Entities;
 using MVVMFirma.Models.EntitiesForView;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NowyAutorViewModel : JedenViewModel<Autorzy>
+    public class NowyAutorViewModel : JedenViewModel<Autorzy>, INotifyDataErrorInfo
     {
         #region Konstruktor
         public NowyAutorViewModel()
                     : base("Dodaj autora")
         {
             item = new Autorzy();
+            ValidateAllProperties();
+
         }
         #endregion
 
@@ -30,7 +34,17 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(Imie));
                 item.Imie = value;
+
+                if (String.IsNullOrEmpty(Imie))
+                {
+                    AddError(nameof(Imie), "Pole imie nie może być puste");
+                } else if (!char.IsUpper(Imie[0]))
+                {
+                    AddError(nameof(Imie), "Pole imie musi zaczynać się od Wielkiej litery");
+                }
+
                 OnPropertyChanged(() => Imie);
             }
         }
@@ -43,7 +57,14 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(DataUrodzenia));
                 item.DataUrodzenia = value;
+
+                if (DataUrodzenia > DateTime.Now)
+                {
+                    AddError(nameof(DataUrodzenia), "Data nie może być w przyszłości");
+                }
+
                 OnPropertyChanged(() => DataUrodzenia);
             }
         }
@@ -56,7 +77,17 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(Nazwisko));
                 item.Nazwisko = value;
+
+                if (String.IsNullOrEmpty(Nazwisko))
+                {
+                    AddError(nameof(Nazwisko), "Pole nazwisko nie może być puste");
+                } else if (!char.IsUpper(Nazwisko[0]))
+                {
+                    AddError(nameof(Nazwisko), "Pole imie musi zaczynać się od Wielkiej litery");
+                }
+
                 OnPropertyChanged(() => Nazwisko);
             }
         }
@@ -86,6 +117,70 @@ namespace MVVMFirma.ViewModels
             bibliotekaEntites.Autorzy.Add(item);//dodaje wypozyczenie do lokalnej kolekcji
             bibliotekaEntites.SaveChanges();//zapisuje zmiany do bazy danych
         }
+        #endregion
+
+        #region Errors
+
+        //errory
+
+        //tworzymy kolekcje:
+        private readonly Dictionary<string, List<string>> _validationMessages = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _validationMessages.Any(); //zmieniamy tutaj na ....Any()
+
+        public new bool IsEnabledSaveButton => !HasErrors;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            //implementujemy nbajpier kolekcje, ktora jest pod koemntem //tworzymy kolekcje nazwa jesj = _validationMessages
+
+            if (_validationMessages.ContainsKey(propertyName))
+            {
+                return _validationMessages[propertyName];//zwracamy dla danego property błąd
+            }
+
+            return null;
+        }
+
+        private void AddError(string propertyName, string validationMessage) //funkcja w seterze do dodawania errorow do kolekcji
+        {
+            if (!_validationMessages.ContainsKey(propertyName))
+            {
+                _validationMessages[propertyName] = new List<string>();
+            }
+
+            _validationMessages[propertyName].Add(validationMessage);
+
+            OnErrorsChanged(propertyName);
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (_validationMessages.ContainsKey(propertyName))
+            {
+                _validationMessages.Remove(propertyName);
+            }
+
+            OnErrorsChanged(propertyName);
+        }
+
+        public void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            OnPropertyChanged(() => IsEnabledSaveButton);
+        }
+
+        //na końcu validacje implementujemy w setterach set
+
+        //wymuszenie walidacji na początku
+        private void ValidateAllProperties()
+        {
+            AddError(nameof(Imie), "Pole imie nie może być puste");
+            AddError(nameof(Nazwisko), "Pole nazwisko nie może być puste");
+        }
+
         #endregion
     }
 }
--- MVVMFirma/ViewModels/NowyEgzemplarzViewModel.cs
+++ MVVMFirma/ViewModels/NowyEgzemplarzViewModel.cs
@@ -2,20 +2,23 @@
 using MVVMFirma.Models.Entities;
 using MVVMFirma.Models.EntitiesForView;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NowyEgzemplarzViewModel : JedenViewModel<Egzemplarze>
+    public class NowyEgzemplarzViewModel : JedenViewModel<Egzemplarze>, INotifyDataErrorInfo
     {
         #region Konstruktor
         public NowyEgzemplarzViewModel()
                     : base("Dodaj egzemplarz")
         {
             item = new Egzemplarze();
+            ValidateAllProperties();
         }
         #endregion
 
@@ -30,7 +33,14 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(IdKsiazki));
                 item.IdKsiazki = value;
+
+                if (IdKsiazki == null)
+                {
+                    AddError(nameof(IdKsiazki), "Proszę wybrać czytelnika");
+                }
+
                 OnPropertyChanged(() => IdKsiazki);
             }
         }
@@ -43,7 +53,14 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(NumerWewnetrznyEgzemplarzu));
                 item.NumerWewnetrznyEgzemplarzu = value;
+
+                if (String.IsNullOrEmpty(NumerWewnetrznyEgzemplarzu))
+                {
+                    AddError(nameof(NumerWewnetrznyEgzemplarzu), "Pole nie może być puste");
+                }
+
                 OnPropertyChanged(() => NumerWewnetrznyEgzemplarzu);
             }
         }
@@ -56,7 +73,14 @@ namespace MVVMFirma.ViewModels
             }
             set
             {
+                ClearErrors(nameof(Stan));
                 item.Stan = value;
+
+                if (String.IsNullOrEmpty(Stan))
+                {
+                    AddError(nameof(Stan), "Pole nie może być puste");
+                }
+
                 OnPropertyChanged(() => Stan);
             }
         }
@@ -83,5 +107,71 @@ namespace MVVMFirma.ViewModels
             bibliotekaEntites.SaveChanges();//zapisuje zmiany do bazy danych
         }
         #endregion
+
+        #region Errors
+
+        //errory
+
+        //tworzymy kolekcje:
+        private readonly Dictionary<string, List<string>> _validationMessages = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _validationMessages.Any(); //zmieniamy tutaj na ....Any()
+
+        public new bool IsEnabledSaveButton => !HasErrors;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            //implementujemy nbajpier kolekcje, ktora jest pod koemntem //tworzymy kolekcje nazwa jesj = _validationMessages
+
+            if (_validationMessages.ContainsKey(propertyName))
+            {
+                return _validationMessages[propertyName];//zwracamy dla danego property błąd
+            }
+
+            return null;
+        }
+
+        private void AddError(string propertyName, string validationMessage) //funkcja w seterze do dodawania errorow do kolekcji
+        {
+            if (!_validationMessages.ContainsKey(propertyName))
+            {
+                _validationMessages[propertyName] = new List<string>();
+            }
+
+            _validationMessages[propertyName].Add(validationMessage);
+
+            OnErrorsChanged(propertyName);
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (_validationMessages.ContainsKey(propertyName))
+            {
+                _validationMessages.Remove(propertyName);
+            }
+
+            OnErrorsChanged(propertyName);
+        }
+
+        public void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            OnPropertyChanged(() => IsEnabledSaveButton);
+        }
+
+        //na końcu validacje implementujemy w setterach set
+
+        //wymuszenie walidacji na początku
+        private void ValidateAllProperties()
+        {
+            AddError(nameof(IdKsiazki), "Proszę wybrać książkę");
+            AddError(nameof(Stan), "Pole Stan nie może być puste");
+            AddError(nameof(NumerWewnetrznyEgzemplarzu), "Numer wewnętrzny nie może być pusty");
+        }
+
+        #endregion
+
     }
 }
